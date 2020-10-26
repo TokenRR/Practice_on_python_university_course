60c45bcb2e0f8a396efd726cf8442929c9508550
unknown <nikitaromanetskiy@gmail.com>
2020-10-26 20:49:59 +0200

    practice5

--- ex1_version1.py
+++
@@ -1,4 +0,0 @@
-products_list = [29.25, 48.99, 99.98, 124.65, 214.30, 543.90, 799.85]
-print('Discount table:')
-for i in products_list:
-    print('{:.2f} {:.2f} {:.2f}'.format(i, i*0.4, i*0.6))
--- ex1_version2.py
+++
@@ -1,9 +0,0 @@
-products_list = [29.25, 48.99, 99.98, 124.65, 214.30, 543.90, 799.85]
-print("Discount table:")
-list_1=[]
-list_2=[]
-for i in products_list:
-    list_1.append(round((i*0.6),2))
-    list_2.append((i-round((i*0.6),2)))
-for i in range(len(list_1)):
-    print(round(products_list[i],2),list_2[i],list_1[i])
